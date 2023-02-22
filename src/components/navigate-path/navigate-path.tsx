0201717c34a620a62f7a7c91c238c14077a83a4d
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2023-02-22 23:29:22 +0300

    Added logic useNavigate

--- src/components/navigate-path/navigate-path.tsx
+++ src/components/navigate-path/navigate-path.tsx
@@ -1,3 +1,11 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import { useNavigate } from 'react-router-dom';
+import { selectedCategorySelector } from '../../selectors';
+import { getBooks } from '../../store/books';
+import { useAppDispatch, useAppSelector } from '../hooks';
+
 import styles from './navigate-path.module.scss';
 
 type PathProps = {
@@ -5,11 +13,23 @@ type PathProps = {
   title: string;
 };
 
-export const NavigatePath = ({ categories, title }: PathProps) => (
-  <div className={styles.navigatePath}>
-    <div className={styles.container}>
-      <span data-test-id='breadcrumbs-link'>{categories.length >= 1 ? categories[0] : 'Все книги'}</span> <span>/</span>{' '}
-      <span data-test-id='book-name'>{title}</span>
+export const NavigatePath = ({ categories, title }: PathProps) => {
+  const { selectedCategory } = useAppSelector(selectedCategorySelector);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const goBackUrl = () => {
+    dispatch(getBooks());
+    navigate(-1);
+  };
+
+  return (
+    <div className={styles.navigatePath}>
+      <div className={styles.container}>
+        <span data-test-id='breadcrumbs-link' onClick={() => goBackUrl()} style={{ cursor: 'pointer' }}>
+          {selectedCategory ? selectedCategory : 'Все книги'}
+        </span>{' '}
+        <span>/</span> <span data-test-id='book-name'>{title}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
