54fcca4e9f5e98fbdf67f1531df129a940e5b573
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2023-03-19 17:04:00 +0300

    Reset order date after response

--- src/components/booking/calendar.tsx
+++ src/components/booking/calendar.tsx
@@ -65,7 +65,9 @@ export const Calendar = ({ isOpen, setIsOpen, bookId }: ModalFromState) => {
       })
     );
     dispatch(clearDateOrder());
+    setSelectedDay(new Date());
     reset();
+    window.scrollTo(0, 0);
   };
 
   const CancelBooking = () => {
